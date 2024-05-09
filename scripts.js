6cd465d5ed399f3297480f0112de93f911df96d8
Haleigh <haleighvalentine@gmail.com>
2024-05-10 00:35:43 +0200

    Resolve user story one

--- scripts.js
+++ scripts.js
@@ -2,8 +2,15 @@ const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+    event.preventDefault();
+    const entries = new FormData(event.target);
+    const { dividend, divider } = Object.fromEntries(entries);
+
+    //Convert input value from string to number
+    const dividendValue = parseFloat(dividend);
+    const dividerValue = parseFloat(divider);
+
+    //Calculate
+    const divisionResult = dividendValue / dividerValue;
+    result.innerText = Math.floor(divisionResult);
 });
